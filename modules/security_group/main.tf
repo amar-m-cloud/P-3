0fb3820e093357f6d679d6a3e90f17f7e8d75254
Amar Mattaparthi <89722343+amar-m-cloud@users.noreply.github.com>
2023-10-16 12:48:06 +0530

    Create main.tf

---
+++ modules/security_group/main.tf
@@ -0,0 +1,16 @@
+variable "vpc_id" {}
+
+resource "aws_security_group" "main" {
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "security_group_id" {
+  value = aws_security_group.main.id
+}
